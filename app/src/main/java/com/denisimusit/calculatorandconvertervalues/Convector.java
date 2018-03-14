74c277d6a7d4f7ce7436970ac4b1923a90fa9ee7
denisimus <deoniisii@gmail.com>
2018-03-14 15:46:36 +0200

    mace convector Dizane

--- app/src/main/java/com/denisimusit/calculatorandconvertervalues/Convector.java
+++ app/src/main/java/com/denisimusit/calculatorandconvertervalues/Convector.java
@@ -2,6 +2,7 @@ package com.denisimusit.calculatorandconvertervalues;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Convector extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Convector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convector);
+    }
+
+    public void onClick(View view) {
     }
 }
